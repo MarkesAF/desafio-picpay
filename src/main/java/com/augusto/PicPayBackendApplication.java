467ee7b2d2e0d2b55e6075f4290dc0e116e45fd9
Augusto Marques <markesaf16@gmail.com>
2023-10-04 21:28:22 -0300

    Começando o Projeto

---
+++ src/main/java/com/augusto/PicPayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.augusto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPayBackendApplication.class, args);
+	}
+
+}
